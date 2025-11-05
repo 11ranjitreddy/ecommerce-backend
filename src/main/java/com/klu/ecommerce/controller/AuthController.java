24a88eb20aa6eae79fa2052f52f253a188e842f1
11ranjitreddy <150681516+11ranjitreddy@users.noreply.github.com>
2025-11-05 19:50:35 +0530

    Update CORS policy to allow all origins

--- src/main/java/com/klu/ecommerce/controller/AuthController.java
+++ src/main/java/com/klu/ecommerce/controller/AuthController.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"http://localhost:30082", "http://localhost:30082/"})
+@CrossOrigin("*")
 public class AuthController {
     private final UserService userService;
 
